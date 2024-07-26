b18753bf8b9d3f41c5358d569cb14228962e3be3
lindellcarternyc <lindellcarternyc@gmail.com>
2024-07-26 03:19:12 -0400

    implements App Gradient

---
+++ src/components/AppGradient.tsx
@@ -0,0 +1,30 @@
+import { ReactNode, useEffect, useRef } from "react";
+
+import styles from "./AppGradient.module.css";
+
+interface LinearGradientProps {
+  children: ReactNode;
+  colors: string[];
+  className?: string;
+}
+
+export default function LinearGradient({
+  children,
+  colors,
+}: LinearGradientProps) {
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    divRef?.current?.style.setProperty(
+      "--gradient",
+      `linear-gradient(${colors.join(", ")})`
+    );
+    console.log(divRef);
+  }, [colors]);
+
+  return (
+    <div className={styles.container} ref={divRef}>
+      {children}
+    </div>
+  );
+}
